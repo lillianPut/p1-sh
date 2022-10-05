f758df90e125c021d25a9e57a8dc2192ae775553
Amanuel Teshome <teshomat@dukes.jmu.edu>
2022-10-05 16:26:29 -0400

    ls and head worked on

--- utils/head.c
+++ utils/head.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
 // You may assume that lines are no longer than 1024 bytes
 #define LINELEN 1024
@@ -9,7 +13,53 @@ static void usage (void);
 int
 main (int argc, char *argv[])
 {
-  return EXIT_SUCCESS;
+  int c;
+  long ret = 0;
+  char *num = NULL;
+  int fname = 0;
+  while ((c = getopt (argc, argv, "n:c")) != -1)
+    {
+      switch (c)
+        {
+          case 'c':
+            exit(EXIT_FAILURE);
+          case 'n':
+            num = optarg;
+            ret = strtoul(num, NULL, 10);
+            break;
+          default:
+            usage();
+            exit(EXIT_FAILURE);
+        }
+        
+    }
+  
+  if (argc != optind)
+    {
+      char *name = argv[optind];
+      fname = open(name, O_RDONLY);
+    }
+    else {
+      fname = STDIN_FILENO;
+    }
+
+  if (ret == 0)
+  {
+    ret = 5;
+  }
+
+  int count = 0;
+  char buffer[LINELEN];
+  read(fname, buffer, LINELEN);
+  char *token = strtok(buffer, "\n");
+  while (count < ret && token != NULL)
+  {
+    printf("%s\n", token);
+    token = strtok(NULL, "\n");
+    count++;
+  }
+  close(fname);
+  exit(EXIT_SUCCESS);
 }
 
 static void
--- utils/ls.c
+++ utils/ls.c
@@ -2,18 +2,82 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <limits.h>
+#include <sys/stat.h>
+#include <string.h>
 
 static void usage (void);
+int parse_args (int argc, char **argv, bool *file_all, bool *sizes, char **fname);
 
 int
 main (int argc, char *argv[])
 {
-  chdir(argv[1]);
-  pid_t child;
-  child = fork();
-  if (child == 0) 
-	  execlp ("ls", "-ls", (char *) NULL);
+  printf ("$");
+  for (int i = 0; i < argc; i++)
+    {
+      printf (" %s", argv[i]);
+    }
+
+  bool a = false;
+  bool s = false;
+  DIR *dir = NULL;
+  char *filename = NULL;
+
+  if (parse_args(argc, argv, &a, &s, &filename) == -1) {
+    usage();
+    return EXIT_FAILURE;
+  }
+  dir = opendir(filename);
+  if (dir == NULL) {
+    return EXIT_FAILURE;
+  }
+
+  char d[100];
+  getcwd(d, sizeof(d));
+  strncat (d, "/", 2);
+  if (argc == 2)
+    strncat (d, argv[1], 10);
+  if (argc == 3)
+    strncat (d, argv[2], 10);
+
+  int hid = 0;
+  helper(a, s, dir, hid, d);
   return EXIT_SUCCESS;
+}
+
+void helper(bool a, bool s, DIR *dir, int hid, char *d) {
+  char *directory = strtok (d, "\n");
+  struct dirent *entry;
+  int index = 0;
+  while ((entry = readdir(dir)) != NULL) {
+    struct stat st;
+    char d2[strlen (directory) + strlen (entry->d_name) + 1];
+    snprintf (d2, strlen (directory) + strlen (entry->d_name) + 2, "%s/%s", directory, entry->d_name);
+    if (a == true && s == true) {
+        stat(d2, &st);
+        int size = st.st_size;
+        printf("%d %s\n", size, entry -> d_name);
+    } else if (s && !a) {
+      if (hid > 1 && entry->d_name[0] != '.')
+      {
+        stat(d2, &st);
+        int size = st.st_size;
+        printf("%d %s\n", size, entry -> d_name);
+      }
+    } else if (!s && a) {
+        printf("%s\n", entry -> d_name);
+    } else {
+       if (entry->d_name[0] != '.' && hid > 1) {
+         printf("%s\n", entry -> d_name);
+       }
+    }
+     hid++;
+     index++;
+  }
 }
 
 static void
@@ -25,4 +89,43 @@ usage (void)
   printf ("  -a       list all files (even hidden ones)\n");
   printf ("  -s       list file sizes\n");
   printf ("If no DIR specified, list current directory contents\n");
+}
+
+int
+parse_args (int argc, char **argv, bool *a, bool *s, char **fname) {
+  int c;
+  while ((c = getopt(argc, argv, "a::s")) != -1) {
+       switch(c) {
+           case 's':
+              *s = true;
+              *fname = optarg;
+              break;
+           case 'a':
+              *a = true;
+              *fname = optarg;
+              break;
+           default:
+              *s = false;
+              *a = false;
+              break;
+       }
+  }
+  if (*fname == NULL) {
+    char cw[100];
+    if (getcwd(cw, sizeof(cw)) != NULL) {
+      if (argc == 2)
+        {
+          strncat (cw, "/", 2);
+          strncat (cw, argv[1], 10);
+          *fname = strtok (cw, "\n");
+        }
+      else if (argc == 3)
+        {
+          strncat (cw, "/", 2);
+          strncat (cw, argv[2], 10);
+          *fname = strtok (cw, "\n");
+        }
+    }
+  }
+  return 0;
 }
